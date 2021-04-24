5b18ea2951e15c5696f5544603e196c3721dd257
João Victor Franco Fernandes <jvfernandes234@gmail.com>
2021-04-23 22:43:06 -0300

    unespMAIL v1
    Esse script utiliza do email e da senha do unespiano para visualizar todos os emails recebidos pelo sisgrad, tirando aquela notificação chata em negrito que sempre aparece.
    #Unesp #Sisgrad #Email #Limpa #Ler

---
+++ unespMAIL.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+import math
+
+email = ""  # Coloque aqui seu email entre aspas
+senha = ""  # Coloque aqui sua senha entre aspas
+timer = 1  # Tempo de espera em segundos, se sua internet for lenta, aumente esse número.
+
+driver = webdriver.Chrome()
+# Página de login ------------------------------------------------------------------------------------------------------
+driver.get('https://sistemas.unesp.br/central/#/sistemas')
+driver.find_element_by_xpath('//*[@id="input_0"]').send_keys(email)
+driver.find_element_by_xpath('//*[@id="input_1"]').send_keys(senha)
+driver.find_element_by_xpath('//*[@id="form"]/md-card/button').click()
+# Página incial do sistema ---------------------------------------------------------------------------------------------
+time.sleep(timer)
+driver.find_element_by_xpath('//*[@id="content"]/md-content/md-grid-list/md-grid-tile[1]/figure/a').click()
+# Sisgrad --------------------------------------------------------------------------------------------------------------
+time.sleep(timer)
+driver.find_element_by_xpath('//*[@id="menuesq"]/li[8]/a').click()
+driver.find_element_by_xpath('//*[@id="menuesq"]/li[8]/ul/li[1]/a').click()
+# Mensagens recebidas --------------------------------------------------------------------------------------------------
+msg_totais_txt = driver.find_element_by_xpath('//*[@id="form_mensagens"]/div/span[1]').text.split(" ")
+msg_totais_int = int(msg_totais_txt[0].replace(".", ""))
+pag_totais = math.ceil(msg_totais_int / 20)
+
+for y in range(1, pag_totais+1):
+    driver.get('https://sistemas.unesp.br/academico/mensagem.listar.action?emailTipo=recebidas&d-4827107-p=' + str(y))
+    for x in range(1, 20):
+        time.sleep(timer)
+        driver.find_element_by_xpath('//*[@id="destinatario"]/tbody/tr[' + str(x) + ']/td[4]/a').click()
+        time.sleep(timer)
+        driver.get('https://sistemas.unesp.br/academico/mensagem.listar.action?emailTipo=recebidas&d-4827107-p='+str(y))
+    time.sleep(timer)
